a40967a670e8e7d39cfab86cbdf465df222cf339
mohammadthajudd <20me0109@vvce.ac.in>
2021-03-08 11:34:29 +0000

    S

--- Program-10.c
+++ Program-10.c
@@ -9,8 +9,8 @@ int main()
     int arr[100], n, i, small, large;
     printf("Enter how many values you want to read and values");
     scanf("%d", &n);
-
-    for (i = 0; i < n; i++)
+    printf("enter the value of a[%d] : ",i);
+        for (i = 0; i < n; i++)
     {
         //printf("Enter the value of a[%d] : ",i);
         scanf("%d", &arr[i]);
@@ -32,9 +32,8 @@ int main()
         }
     }
 
-    printf("The largest element of the array are %d" , large);
-    printf("The smallest element of the array are %d",small);
-
+    printf("The largest and smallest elements are %d and %d " , large,small);
+  
     return 0;
 }
 
